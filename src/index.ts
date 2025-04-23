5abff4effc91bc00d3ed2f51597689509f237acb
drain <drain.cy.chang@gmail.com>
2025-04-23 11:13:44 +0800

    basic support of moving window left, right, and up

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import {
     isApplicationWindow,
     cleanUpTiles,
     log,
-    logTileTreeInfo,
+    logTileTreeInfo, windowForTile,
 } from "./util";
 import {TileHelper} from "./tile_helper";
 import {windowMinimizedChangedHandler, windowOutputChangedHandler} from "./signal_handlers";
+import {LayoutDirection} from "./layout_direction";
 
 log("------------------ new geto kwin script session started ------------------")
 
@@ -30,3 +31,76 @@ workspace.windowList().forEach((window) => {
 })
 
 TileHelper.resetAllWindowTiles()
+
+const enlargeWindow = () => {
+    log("enlarge window")
+}
+const moveWindowLeft = () => {
+    const activeWindow = workspace.activeWindow
+    const tile = workspace.activeWindow?.tile
+    const parentTile = tile?.parent
+    const sameLevelTiles = parentTile?.tiles
+    if (activeWindow && tile && parentTile && sameLevelTiles?.length) {
+        const currentWindowIndex = sameLevelTiles.indexOf(tile)
+        const otherWindowIndex = (currentWindowIndex + 1) % 2
+        const otherWindowTile = sameLevelTiles[otherWindowIndex];
+        const otherWindow = windowForTile(otherWindowTile)
+        if (otherWindow && otherWindowTile.absoluteGeometry.x < tile.absoluteGeometry.x) {
+            otherWindow.tile = null
+            activeWindow.tile = null
+            otherWindow.tile = tile
+            activeWindow.tile = otherWindowTile
+        }
+    }
+}
+const moveWindowRight = () => {
+    const activeWindow = workspace.activeWindow
+    const tile = workspace.activeWindow?.tile
+    const parentTile = tile?.parent
+    const sameLevelTiles = parentTile?.tiles
+    if (activeWindow && tile && parentTile && sameLevelTiles?.length) {
+        const currentWindowIndex = sameLevelTiles.indexOf(tile)
+        const otherWindowIndex = (currentWindowIndex + 1) % 2
+        const otherWindowTile = sameLevelTiles[otherWindowIndex];
+        const otherWindow = windowForTile(otherWindowTile)
+        if (otherWindow && otherWindowTile.absoluteGeometry.x > tile.absoluteGeometry.x) {
+            otherWindow.tile = null
+            activeWindow.tile = null
+            otherWindow.tile = tile
+            activeWindow.tile = otherWindowTile
+        }
+    }
+}
+const moveWindowUp = () => {
+    const activeWindow = workspace.activeWindow
+    const tile = workspace.activeWindow?.tile
+    const parentTile = tile?.parent
+    const sameLevelTiles = parentTile?.tiles
+    if (activeWindow && tile && parentTile && sameLevelTiles?.length) {
+        const currentWindowIndex = sameLevelTiles.indexOf(tile)
+        const otherWindowIndex = (currentWindowIndex + 1) % 2
+        const otherWindowTile = sameLevelTiles[otherWindowIndex];
+        const otherWindow = windowForTile(otherWindowTile)
+        if (otherWindow && otherWindowTile.absoluteGeometry.y === tile.absoluteGeometry.y) {
+            otherWindow.tile = null
+            activeWindow.tile = null
+            tile.remove()
+            otherWindowTile.remove()
+            const childTiles = parentTile.split(LayoutDirection.Vertical)
+            otherWindow.tile = childTiles[0]
+            activeWindow.tile = childTiles[1]
+            const screen = activeWindow.output
+            const tileManager = workspace.tilingForScreen(screen)
+            logTileTreeInfo(tileManager.rootTile)
+        }
+    }
+}
+const moveWindowDown = () => {
+    log("moveWindowDown")
+}
+
+registerShortcut("EnlargeWindow", "EnlargeWindow", "", enlargeWindow)
+registerShortcut("MoveWindowLeft", "MoveWindowLeft", "", moveWindowLeft)
+registerShortcut("MoveWindowRight", "MoveWindowRight", "", moveWindowRight)
+registerShortcut("MoveWindowUp", "MoveWindowUp", "", moveWindowUp)
+registerShortcut("MoveWindowDown", "MoveWindowDown", "", moveWindowDown)
