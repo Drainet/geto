5e1063600d3deef25013d0e1849033d1c025d3b8
drain <drain.cy.chang@gmail.com>
2025-12-30 16:36:53 +0800

    remove no window tiles after workspace's window added or removed

--- src/index.ts
+++ src/index.ts
@@ -1,115 +1,139 @@
-import {isApplicationWindow, log, TileUtil} from "./util";
-import {TileMode} from "./tile_helper";
-import {windowMinimizedChangedHandler, windowOutputChangedHandler} from "./signal_handlers";
-import {TileHelperProvider} from "./tile_helper/tile_helper_provider";
+import { isApplicationWindow, log, TileUtil } from "./util";
+import { TileMode } from "./tile_helper";
+import {
+  windowMinimizedChangedHandler,
+  windowOutputChangedHandler,
+} from "./signal_handlers";
+import { TileHelperProvider } from "./tile_helper/tile_helper_provider";
 
-log("------------------ new geto kwin script session started ------------------")
+log(
+  "------------------ new geto kwin script session started ------------------",
+);
 
-let currentTileMode = TileMode.Default
+let currentTileMode = TileMode.Default;
 
 workspace.windowAdded.connect((window) => {
-    if (isApplicationWindow(window)) {
-        TileHelperProvider[currentTileMode].addWindowToScreen({window})
-        window.minimizedChanged.connect(windowMinimizedChangedHandler({
-            window,
-            tileHelperProvider: TileHelperProvider,
-            currentTileModeProvider: () => {
-                return currentTileMode
-            }
-        }))
-        window.outputChanged.connect(windowOutputChangedHandler({
-            window,
-            tileHelperProvider: TileHelperProvider,
-            currentTileModeProvider: () => {
-                return currentTileMode
-            }
-        }))
-    }
-})
+  if (isApplicationWindow(window)) {
+    const tileHelper = TileHelperProvider[currentTileMode];
+    tileHelper.addWindowToScreen({ window });
+    window.minimizedChanged.connect(
+      windowMinimizedChangedHandler({
+        window,
+        tileHelperProvider: TileHelperProvider,
+        currentTileModeProvider: () => {
+          return currentTileMode;
+        },
+      }),
+    );
+    window.outputChanged.connect(
+      windowOutputChangedHandler({
+        window,
+        tileHelperProvider: TileHelperProvider,
+        currentTileModeProvider: () => {
+          return currentTileMode;
+        },
+      }),
+    );
+    TileUtil.removeNoWindowTiles(tileHelper);
+  }
+});
 
 workspace.windowRemoved.connect((window) => {
-    if (isApplicationWindow(window)) {
-        TileHelperProvider[currentTileMode].removeFromTile({window})
-    }
-})
+  if (isApplicationWindow(window)) {
+    const tileHelper = TileHelperProvider[currentTileMode];
+    tileHelper.removeFromTile({ window });
+    TileUtil.removeNoWindowTiles(tileHelper);
+  }
+});
 
 workspace.windowActivated.connect((window) => {
-    if (isApplicationWindow(window)) {
-        window.lastActivatedDate = new Date()
-        TileHelperProvider[currentTileMode].handleWindowActivated(window)
-    }
-})
+  if (isApplicationWindow(window)) {
+    window.lastActivatedDate = new Date();
+    TileHelperProvider[currentTileMode].handleWindowActivated(window);
+  }
+});
 
 workspace.windowList().forEach((window) => {
-    window.tile = null
-    window.minimizedChanged.connect(windowMinimizedChangedHandler({
-        window,
-        tileHelperProvider: TileHelperProvider,
-        currentTileModeProvider: () => {
-            return currentTileMode
-        }
-    }))
-    window.outputChanged.connect(windowOutputChangedHandler({
-        window,
-        tileHelperProvider: TileHelperProvider,
-        currentTileModeProvider: () => {
-            return currentTileMode
-        }
-    }))
-})
-
+  window.tile = null;
+  window.minimizedChanged.connect(
+    windowMinimizedChangedHandler({
+      window,
+      tileHelperProvider: TileHelperProvider,
+      currentTileModeProvider: () => {
+        return currentTileMode;
+      },
+    }),
+  );
+  window.outputChanged.connect(
+    windowOutputChangedHandler({
+      window,
+      tileHelperProvider: TileHelperProvider,
+      currentTileModeProvider: () => {
+        return currentTileMode;
+      },
+    }),
+  );
+});
 
 const enlargeWindow = () => {
-    log("enlarge window")
-}
+  log("enlarge window");
+};
 
 const unMinimizePrevMinimizedWindow = () => {
-    const minimizedWindows = workspace.windowList()
-        .filter(isApplicationWindow)
-        .filter((window) => window.minimized)
-    let targetWindow: Window | undefined
-    for (let i = 0; i < minimizedWindows.length; i++) {
-        const current = minimizedWindows[i]
-        if ((current.minimizedDate?.getTime() ?? 0) > (targetWindow?.minimizedDate?.getTime() ?? 0)) {
-            targetWindow = current
-        }
+  const minimizedWindows = workspace
+    .windowList()
+    .filter(isApplicationWindow)
+    .filter((window) => window.minimized);
+  let targetWindow: Window | undefined;
+  for (let i = 0; i < minimizedWindows.length; i++) {
+    const current = minimizedWindows[i];
+    if (
+      (current.minimizedDate?.getTime() ?? 0) >
+      (targetWindow?.minimizedDate?.getTime() ?? 0)
+    ) {
+      targetWindow = current;
     }
-    if (targetWindow) {
-        targetWindow.minimized = false
-    }
-}
-
+  }
+  if (targetWindow) {
+    targetWindow.minimized = false;
+  }
+};
 
 const switchMode = () => {
-    workspace.screens.forEach((screen) => {
-        const tileManager = workspace.tilingForScreen(screen)
-        TileUtil.cleanUpTiles(tileManager.rootTile)
-    })
-    if (currentTileMode === TileMode.Default) {
-        currentTileMode = TileMode.Queue
-    } else {
-        currentTileMode = TileMode.Default
-    }
-    TileUtil.resetAllWindowTiles(TileHelperProvider[currentTileMode])
-}
+  workspace.screens.forEach((screen) => {
+    const tileManager = workspace.tilingForScreen(screen);
+    TileUtil.cleanUpTiles(tileManager.rootTile);
+  });
+  if (currentTileMode === TileMode.Default) {
+    currentTileMode = TileMode.Queue;
+  } else {
+    currentTileMode = TileMode.Default;
+  }
+  TileUtil.resetAllWindowTiles(TileHelperProvider[currentTileMode]);
+};
 
-registerShortcut("GetoEnlargeWindow", "GetoEnlargeWindow", "", enlargeWindow)
+registerShortcut("GetoEnlargeWindow", "GetoEnlargeWindow", "", enlargeWindow);
 registerShortcut("GetoMoveWindowLeft", "GetoMoveWindowLeft", "", () => {
-    TileHelperProvider[currentTileMode].moveWindowLeft()
-})
+  TileHelperProvider[currentTileMode].moveWindowLeft();
+});
 registerShortcut("GetoMoveWindowRight", "GetoMoveWindowRight", "", () => {
-    TileHelperProvider[currentTileMode].moveWindowRight()
-})
+  TileHelperProvider[currentTileMode].moveWindowRight();
+});
 registerShortcut("GetoMoveWindowUp", "GetoMoveWindowUp", "", () => {
-    TileHelperProvider[currentTileMode].moveWindowUp()
-})
+  TileHelperProvider[currentTileMode].moveWindowUp();
+});
 registerShortcut("GetoMoveWindowDown", "GetoMoveWindowDown", "", () => {
-    TileHelperProvider[currentTileMode].moveWindowDown()
-})
+  TileHelperProvider[currentTileMode].moveWindowDown();
+});
 registerShortcut("GetoResetAllTiles", "GetoResetAllTiles", "", () => {
-    TileUtil.resetAllWindowTiles(TileHelperProvider[currentTileMode])
-})
-registerShortcut("GetoUnMinimizePrevMinimizedWindow", "GetoUnMinimizePrevMinimizedWindow", "", unMinimizePrevMinimizedWindow)
-registerShortcut("GetoSwitchMode", "GetoSwitchMode", "", switchMode)
+  TileUtil.resetAllWindowTiles(TileHelperProvider[currentTileMode]);
+});
+registerShortcut(
+  "GetoUnMinimizePrevMinimizedWindow",
+  "GetoUnMinimizePrevMinimizedWindow",
+  "",
+  unMinimizePrevMinimizedWindow,
+);
+registerShortcut("GetoSwitchMode", "GetoSwitchMode", "", switchMode);
 
-TileUtil.resetAllWindowTiles(TileHelperProvider[currentTileMode])
+TileUtil.resetAllWindowTiles(TileHelperProvider[currentTileMode]);
